d0c933a99b64d55aeffed0a5e861c886e517c91f
Kevin Su <111539270+commiterate@users.noreply.github.com>
2026-08-05 16:29:20 -0400

    chore: Upgrade AIStore Nix package to 1.4.9. (#128)

--- nix/overlays/development/multi-storage-client/packages/aistore/package.nix
+++ nix/overlays/development/multi-storage-client/packages/aistore/package.nix
@@ -7,20 +7,20 @@
 # https://nixos.org/manual/nixpkgs/unstable#ssec-language-go
 buildGoModule (finalAttrs: {
   pname = "aistore";
-  version = "1.4.8";
+  version = "1.4.9";
 
   src = fetchFromGitHub {
     owner = "NVIDIA";
     repo = "aistore";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-u6MBpOQfQh+EuIYlN0jHMgByHPj1TKn/DTzAb1eRwcc=";
+    hash = "sha256-0EmOEexiDnjed7otPx+XWBcUbbDzvgXMsJ1pNEGco9c=";
   };
 
   vendorHash = "sha256-lWsLLIx4YJcitNpHWl965VBRHJDRxw8EWTGBjiexQBE=";
 
   # Exclude `cmd/cli` and `cmd/ishard` which are separate Go modules.
   #
-  # https://github.com/NVIDIA/aistore/tree/v1.4.8/cmd
+  # https://github.com/NVIDIA/aistore/tree/v1.4.9/cmd
   subPackages = [
     "cmd/aisinit"
     "cmd/aisloader"
@@ -32,7 +32,7 @@ buildGoModule (finalAttrs: {
 
   # Needed for version strings.
   #
-  # https://github.com/NVIDIA/aistore/blob/v1.4.8/Makefile#L86
+  # https://github.com/NVIDIA/aistore/blob/v1.4.9/Makefile#L86
   ldflags = [
     "-X main.build=v${finalAttrs.version}"
     "-X main.buildtime=1970-01-01T00:00:00-00:00"
@@ -41,7 +41,7 @@ buildGoModule (finalAttrs: {
   tags = [
     # Monotonic time.
     #
-    # https://github.com/NVIDIA/aistore/blob/v1.4.8/Makefile#L98
+    # https://github.com/NVIDIA/aistore/blob/v1.4.9/Makefile#L98
     "mono"
   ];
 
